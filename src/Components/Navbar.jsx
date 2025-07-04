57bfda669ac13c76dc512293529cc370155fccc7
mayahNeko <mayahibela@gmail.com>
2025-07-04 22:26:01 +0300

    feat: change icon and fix the Navbar wrong marging

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
         <>
-            <div className="bg-[var(--primary-color)] flex flex-row justify-between h-[17vh] mx-3 top-0">
+            <div className="bg-[var(--primary-color)] flex flex-row justify-between h-[17vh] px-3 top-0">
                 <img src="../src/assets/images/hei-logo.png" alt="hei-logo"/>
                 <ul className=" text-white text-xl font-semibold font-[var(--hei-policy)] items-center flex flex-row gap-x-5 mr-5">
                     <li className="text-[var(--secondary-color)]">ACCUEIL</li>
