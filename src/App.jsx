a8da71bbddd5ec54017877f42805f5d118b68618
mayahNeko <mayahibela@gmail.com>
2025-07-04 15:02:56 +0300

    Add the OpenWay part

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./Components/Navbar"
 import About from "./Components/About"
+import OpenWay from "./Components/OpenWay"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div>
         <Navbar/>
         <About/>
+        <OpenWay/>
     </div>
   )
 }
