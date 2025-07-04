2f8bfbcc8a9c5e384ebee720727b483482ebbcaf
mayahNeko <mayahibela@gmail.com>
2025-07-04 20:12:18 +0300

    feat: fix the text-size of the OpenWay field

--- src/Components/OpenWay.jsx
+++ src/Components/OpenWay.jsx
@@ -5,9 +5,9 @@ const OpenWay = () => {
                 <img src="../src/assets/images/hei-image1.webp" alt="hei-image" className="h-[80vh]" />
                 <div className="text-[var(--primary-color)] ">
                     <h1 className="font-semibold text-5xl mb-5 ml-6">Quelques débouchés</h1>
-                    <div className="flex flex-col justify-between gap-5 ml-6 mr-25 text-l">
-                        <p>Formez-vous à une panoplie de métiers du numérique avec nous et venez acquérir les fondements de la programmation (qui vous servira pour les différents métiers) ! Ce sont des métiers porteurs, des métiers du présent, des métiers du futur…</p>
-                        <p>Développeur back end, développeur front end, développeurs mobile, chefs de projet, exploitants cloud, analystes cybersécurité, pentesters, auditeur technique SSI, auditeur organisationnel SSI, administrateur système et réseau, développeur de sécurité…</p>
+                    <div className="flex flex-col justify-between gap-7 ml-6 mr-25">
+                        <p className="text-[20px]">Formez-vous à une panoplie de métiers du numérique avec nous et venez acquérir les fondements de la programmation (qui vous servira pour les différents métiers) ! Ce sont des métiers porteurs, des métiers du présent, des métiers du futur…</p>
+                        <p className="text-[20px]">Développeur back end, développeur front end, développeurs mobile, chefs de projet, exploitants cloud, analystes cybersécurité, pentesters, auditeur technique SSI, auditeur organisationnel SSI, administrateur système et réseau, développeur de sécurité…</p>
                     </div>
                 </div>
             </div>
